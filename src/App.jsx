877dfb301d378bff88374505dee69d56e16b27f7
Gayathri <g3saga5335@gmail.com>
2025-01-18 01:35:27 +0530

    Committing all files to repo

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
     
     try {
       // Sending the form data to the backend
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://air-quality-react-backend.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
